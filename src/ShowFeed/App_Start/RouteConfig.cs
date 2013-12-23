a72b02ccb05e1da0a67aecca25e6cb7b739e4c55
Thomas Meyer <strayfatty@gmail.com>
2013-12-23 22:34:36 +0100

    Create empty mvc project.

---
+++ src/ShowFeed/App_Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace ShowFeed.App_Start
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    /// <summary>
+    /// The route configuration class.
+    /// </summary>
+    public static class RouteConfig
+    {
+        /// <summary>
+        /// Registers all routes.
+        /// </summary>
+        /// <param name="routes">The route collection.</param>
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
---
+++ src/ShowFeed/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿namespace ShowFeed
+{
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using ShowFeed.App_Start;
+
+    /// <summary>
+    /// The MVC application class.
+    /// </summary>
+    public class MvcApplication : HttpApplication
+    {
+        /// <summary>
+        /// The application start event handler.
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
