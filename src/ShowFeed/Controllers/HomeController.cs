e38de94764a386232a0cea14eeb38cf53212a3e3
Thomas Meyer <strayfatty@gmail.com>
2013-12-23 23:44:01 +0100

    Create home controller and index view.

---
+++ src/ShowFeed/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿namespace ShowFeed.Controllers
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The home controller.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// The index view.
+        /// </summary>
+        /// <returns>An action result.</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
