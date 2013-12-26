91658d67ac557a983311ac5e88a346f897fc1d09
Thomas Meyer <strayfatty@gmail.com>
2013-12-26 21:12:26 +0100

    Account views and actions.

---
+++ src/ShowFeed/ViewModels/AccountLogInViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ShowFeed.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The account sign up view model.
+    /// </summary>
+    public class AccountLogInViewModel
+    {
+        /// <summary>
+        /// Gets or sets the username.
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+    }
+}
