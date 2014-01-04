1f28ba869ca0e6d83b811a642a4a2e379221db1d
Thomas Meyer <strayfatty@gmail.com>
2014-01-04 20:41:23 +0100

    Add updates controller.

---
+++ src/ShowFeed/Controllers/UpdatesController.cs
@@ -0,0 +1,54 @@
+﻿namespace ShowFeed.Controllers
+{
+    using System.Web.Mvc;
+
+    using ShowFeed.Models;
+    using ShowFeed.Services;
+
+    /// <summary>
+    /// The updates controller.
+    /// </summary>
+    public class UpdatesController : Controller
+    {
+        /// <summary>
+        /// The database.
+        /// </summary>
+        private readonly IDatabase database;
+
+        /// <summary>
+        /// The series service.
+        /// </summary>
+        private readonly ISeriesService seriesService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdatesController"/> class.
+        /// </summary>
+        /// <param name="database">The database.</param>
+        /// <param name="seriesService">The series service.</param>
+        public UpdatesController(IDatabase database, ISeriesService seriesService)
+        {
+            this.database = database;
+            this.seriesService = seriesService;
+        }
+
+        /// <summary>
+        /// The index view.
+        /// </summary>
+        /// <returns>An action result.</returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// The new action.
+        /// </summary>
+        /// <returns>The <see cref="ActionResult"/>.</returns>
+        [HttpPost]
+        public ActionResult New()
+        {
+            return this.RedirectToRoute("updates");
+        }
+    }
+}
