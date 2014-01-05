d7c5d83c5529ff92e62e4941f1ab3bb9be7961be
Thomas Meyer <strayfatty@gmail.com>
2014-01-05 19:03:07 +0100

    Series updating.

--- src/ShowFeed/Controllers/UpdatesController.cs
+++ src/ShowFeed/Controllers/UpdatesController.cs
@@ -1,13 +1,18 @@
 ﻿namespace ShowFeed.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
+
+    using AutoMapper;
 
     using ShowFeed.Api.Model;
     using ShowFeed.Models;
     using ShowFeed.Services;
     using ShowFeed.ViewModels;
+
+    using StackExchange.Profiling;
 
     /// <summary>
     /// The updates controller.
@@ -58,7 +63,7 @@
                 {
                     Id = x.Id,
                     Started = CalendarEntry.Epoch.AddSeconds(x.Started),
-                    Duration = new TimeSpan(0, 0, 0, 0, x.Finished - x.Started),
+                    Duration = new TimeSpan(0, 0, x.Finished - x.Started),
                     NumberOfSeriesUpdated = x.NumberOfSeriesUpdated,
                     NumberOfEpisodesUpdated = x.NumberOfEpisodesUpdated
                 })
@@ -74,7 +79,55 @@
         [HttpPost]
         public ActionResult New()
         {
+            var lastUpdate = this.database.Query<Update>()
+                .OrderByDescending(x => x.Started)
+                .FirstOrDefault();
+
+            var startTime = DateTime.UtcNow;
+            var offset = (int)(startTime.AddSeconds(-10) - CalendarEntry.Epoch).TotalSeconds;
+            if (lastUpdate == null || lastUpdate.Started < offset)
+            {
+                var update = new Update();
+                update.Started = (int)(startTime - CalendarEntry.Epoch).TotalSeconds;
+
+                var updateData = this.seriesService.GetUpdateData(lastUpdate != null ? lastUpdate.UpdateTime : 0);
+                this.UpdateSeries(update, updateData.Series);
+                this.UpdateEpisodes(update, updateData.Episodes);
+
+                update.Finished = (int)(DateTime.UtcNow - CalendarEntry.Epoch).TotalSeconds;
+                update.UpdateTime = updateData.UpdateTime;
+
+                this.database.Store(update);
+                this.database.SaveChanges();
+            }
+
             return this.RedirectToRoute("updates");
+        }
+
+        private void UpdateSeries(Update update, IEnumerable<ISeriesUpdateRecord> records)
+        {
+            using (MiniProfiler.Current.Step("update series"))
+            {
+                foreach (var record in records)
+                {
+                    var series = this.database.Load<Series>(record.SeriesId);
+                    if (series == null)
+                    {
+                        continue;
+                    }
+
+                    ////var baseSeriesRecord = this.seriesService.GetBaseSeriesRecord(record.SeriesId);
+                    ////Mapper.Map(baseSeriesRecord, series);
+                    ////update.Series.Add(series);
+                }
+            }
+        }
+
+        private void UpdateEpisodes(Update update, IEnumerable<IEpisodeUpdateRecord> records)
+        {
+            using (MiniProfiler.Current.Step("update series"))
+            {
+            }
         }
     }
 }
