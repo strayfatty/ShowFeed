b10fa55a2feedfd37967156ffb1a560cd1e4b274
Thomas Meyer <strayfatty@gmail.com>
2017-07-29 23:59:30 +0200

    fixup! Complete rewrite

---
+++ src/ShowFeed/Server/Logger.cs
@@ -0,0 +1,46 @@
+﻿namespace ShowFeed.Server
+{
+    using System;
+    using System.Linq;
+
+    using Serilog;
+    using Serilog.Core;
+
+    /// <summary>
+    /// The logger configuration.
+    /// </summary>
+    public static class Logger
+    {
+        /// <summary>
+        /// The logger instance.
+        /// </summary>
+        private static readonly ILogger Instance = new LoggerConfiguration()
+            .ReadFrom.AppSettings()
+            .CreateLogger();
+
+        /// <summary>
+        /// Create a logger that marks log events as being from the specified source type.
+        /// </summary>
+        /// <typeparam name="T">The source type.</typeparam>
+        /// <returns>A logger that will enrich log events as specified.</returns>
+        public static ILogger ForContext<T>()
+        {
+            return Instance.ForContext<T>();
+        }
+
+        /// <summary>
+        /// Create a logger that marks log events as being from the specified source type.
+        /// </summary>
+        /// <param name="source">The source type.</param>
+        /// <returns>A logger that will enrich log events as specified.</returns>
+        public static ILogger ForContext(Type source)
+        {
+            if (source == null)
+            {
+                return Instance.ForContext(Enumerable.Empty<ILogEventEnricher>());
+            }
+
+            return Instance.ForContext(source);
+        }
+    }
+}
--- src/ShowFeed/Server/WebApiConfig.cs
+++ src/ShowFeed/Server/WebApiConfig.cs
@@ -1,9 +1,12 @@
 ﻿namespace ShowFeed.Server
 {
     using System.Web.Http;
+    using System.Web.Http.ExceptionHandling;
 
     using Newtonsoft.Json.Converters;
     using Newtonsoft.Json.Serialization;
+
+    using Serilog;
 
     /// <summary>
     /// The Web API configuration.
@@ -21,6 +24,40 @@
             json.SerializerSettings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
 
             config.MapHttpAttributeRoutes();
+
+            config.Services.Add(typeof(IExceptionLogger), new SerilogExecptionLogger());
+        }
+
+        /// <summary>
+        /// The serilog exception logger.
+        /// </summary>
+        private class SerilogExecptionLogger : ExceptionLogger
+        {
+            /// <summary>
+            /// The logger.
+            /// </summary>
+            private readonly ILogger logger;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="SerilogExecptionLogger"/> class.
+            /// </summary>
+            public SerilogExecptionLogger()
+            {
+                this.logger = Logger.ForContext<SerilogExecptionLogger>();
+            }
+
+            /// <summary>
+            /// When overridden in a derived class, logs the exception synchronously.
+            /// </summary>
+            /// <param name="context">The exception logger context.</param>
+            public override void Log(ExceptionLoggerContext context)
+            {
+                this.logger.Error(
+                    context.Exception,
+                    "Unhandled exception processing {0} for {1}",
+                    context.Request.Method,
+                    context.Request.RequestUri.PathAndQuery);
+            }
         }
     }
 }
