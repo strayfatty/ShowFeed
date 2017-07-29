bf609d92e209863335c79d13a450bc6b4ba63d2e
Thomas Meyer <strayfatty@gmail.com>
2017-07-29 12:23:41 +0200

    fixup! Complete rewrite

---
+++ src/ShowFeed/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿namespace ShowFeed
+{
+    using System;
+    using System.Web;
+
+    /// <summary>
+    /// The global application class.
+    /// </summary>
+    public class Global : HttpApplication
+    {
+        /// <summary>
+        /// The application start event handler.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The event arguments.</param>
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
